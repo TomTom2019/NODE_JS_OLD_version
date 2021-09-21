45676c2339d5dcbbcd3d07343275f9ad87292580
TomTom2019 <daless2000be@yandex.com>
2021-09-21 13:33:04 +0200

    mongoose

--- server.js
+++ server.js
@@ -1,43 +1,26 @@
 const express = require('express');
 const app = express();
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 const mongoUri = 'mongodb+srv://admin:testing123@cluster0.lwqgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-const client = new MongoClient(mongoUri);
+//////////////////////////////////
 
+const carSchema = mongoose.Schema({
+    brand: String,
+    model:String,
+    year: Number,
+    avail: Boolean
+});
 
-
-app.get('/api/users', async(req,res)=>{
-   try {
-    await client.connect();
-    const database = client.db('myApp');
-    const collection = database.collection('users')
-    const query = await collection.insertOne({
-        name:"Francis",
-        lastname:"Jones"
-    });
-
-    console.log(query)
-
-    res.status(200).json({awesome:'yes'});
-   } catch(error) {
-    throw error;
-   } finally {
-    await client.close();
-    console.log('all is done')
-   }
-})
+const Car = mongoose.model('Car',carSchema);
 
 
-
- // MongoClient.connect(mongoUri,{ useUnifiedTopology: true },(err, client)=>{
- //    if(err){
- //         throw err;
- //     }
- //    console.log('connected to the db')
- // })
-
+/////////////////////////////////
 
 const port = process.env.PORT || 3001;
 app.listen(port)

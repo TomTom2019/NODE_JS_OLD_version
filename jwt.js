72ac2b3994750006b2414b4bfd335edd2d6285e2
TomTom2019 <daless2000be@yandex.com>
2021-09-25 13:25:26 +0200

    message

--- jwt.js
+++ jwt.js
@@ -1,12 +1,1 @@
-const jwt = require('jsonwebtoken');
-
-let userData = 100;
-let secret = 'supersecret';
-
-const token = jwt.sign(userData,secret);
-const decodeToken = jwt.verify(token,secret + '2')
-
-
-console.log(token)
-console.log(decodeToken)
 

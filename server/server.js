efcab19627d97b41d823ddf5e598115c9b901ef7
TomTom2019 <daless2000be@yandex.com>
2021-09-27 14:00:49 +0200

    message

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+
+
 const mongoUri = '';
 mongoose.connect(mongoUri,{
     useNewUrlParser: true,
@@ -15,6 +17,31 @@ app.use(bodyParser.json());
 
 /// MODELS
 const { User } = require('./models/user');
+
+/// ROUTES
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc);
+    });
+})
+
+app.post('/api/user/login',(req,res)=>{
+    // 1 - Find the user, if good ->
+    User.findOne({'email': req.body.email},(err, user)=>{
+        if(!user) res.json({message:'User not found'});
+
+        // 2 - compare the string with the hash ->
+        user.comparePassword( req.body.password , (err, isMatch)=>{
+            if(err) res.json({message:'Bad password'});
+            res.status(200).send(isMatch)
+        })
+    })
+});
 
 
 const port = process.env.PORT || 3001;

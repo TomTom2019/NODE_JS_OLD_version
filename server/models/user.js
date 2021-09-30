0a94ab37c71a442b4c1db6a1d9e9defd489e409c
TomTom2019 <daless2000be@yandex.com>
2021-09-30 14:16:02 +0200

    message

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+
 
 const userSchema = mongoose.Schema({
     email: {
@@ -13,8 +15,11 @@ const userSchema = mongoose.Schema({
         type:String,
         required:true,
         minlength: 6
+    },
+    token:{
+        type: String
     }
-})
+});
 
 userSchema.pre('save',function(next){
     var user = this;
@@ -33,12 +38,37 @@ userSchema.pre('save',function(next){
     }
 })
 
+
 userSchema.methods.comparePassword = function(candidatePassword, cb){
     bcrypt.compare(candidatePassword,this.password,function(err, isMatch){
-        if(err) cb(err)
+        if(err) return cb(err)
         cb(null,isMatch)
     })
 }
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'supersecretpassword');
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user)
+    })
+}
+
+userSchema.statics.findByToken = function(token,cb){
+    const user = this;
+
+    jwt.verify(token,'supersecretpassword',(err, decode)=>{
+        user.findOne({'_id':decode,'token':token},(err,user)=>{
+            if(err) return cb(err);
+            cb(null,user);
+        })
+    })
+}
+
+
 
 
 const User = mongoose.model('User',userSchema);
